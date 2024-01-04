8c1c82b433d8cdba553598882dee40c4f6ae01e5
Samuel Abu <abu.sammie@gmail.com>
2024-01-04 16:46:43 +0200

    dark mode theme basic setup

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Switch } from 'react-native';
+import { useColorScheme } from 'nativewind'
 
 export default function App() {
+  const { colorScheme, toggleColorScheme }  = useColorScheme()
+  //console.log(colorScheme)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 bg-white justify-center items-center dark:bg-neutral-900">
+      <StatusBar style={colorScheme == 'dark' ? 'light' : 'dark'} />
+      <View className="flex-row justify-center items-center">
+        <Text className="text-xl dark:text-white">Toggle Theme</Text>
+        <Switch value={colorScheme == 'dark'} onChange={toggleColorScheme} />
+      </View>
+      <Text className="dark:text-white">Open up App.js to start working on your app!</Text>
     </View>
   );
 }
